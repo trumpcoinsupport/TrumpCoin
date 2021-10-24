f3145efb6e194d0875da813f958e9dbd8288e275
Kozisto <sebgruby@gmail.com>
2021-10-24 11:47:06 +0200

    new checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -83,6 +83,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     (750000, uint256("0x7cbfdc7aa6b2d7d2e6753687e98f1a00cdc85add656f7770fcad5a51d1ee9882"))
     (800000, uint256("0x4dfe8ab65c01975161e118dadc6a0af7bf5de09544b3f2c490cff96a87c8dbef"))
     (850000, uint256("0x5c91445906b1dd5bb6f5c85fed647043e26100257c7d69c440e9cc95c50a94e8"))
+    (900000, uint256("0x1757dc2b34dba42cba2a0bf6fa061d576f59556d71b605ff5c05b44850d4cf86"))
     ;
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
